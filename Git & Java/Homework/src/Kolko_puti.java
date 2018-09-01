70a0757c928f2a5de0a9137296ab9b26119ed81d
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-09-01 14:40:21 +0300

    Add solution to Kolko puti

---
+++ Git & Java/Homework/src/Kolko_puti.java
@@ -0,0 +1,27 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Kolko_puti {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(" ");
+        
+        Map<String, Integer> numMap = new LinkedHashMap<>();
+        
+        for (String num : input) {
+            if(numMap.containsKey(num)) {
+                numMap.put(num, numMap.get(num)+1);
+                continue;
+            }
+            numMap.put(num,1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, Integer> KvP : numMap.entrySet()) {
+            sb.append(KvP.getKey() + " " + KvP.getValue() + " ");
+        }
+
+        System.out.printf(sb.toString().trim());
+    }
+}
