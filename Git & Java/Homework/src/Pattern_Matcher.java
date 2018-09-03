a92022addc38e1f55401441792d2cc264f107b1a
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-09-03 21:40:39 +0300

    Add solution to Pattern_Matcher

--- Git & Java/Homework/src/Pattern_Matcher.java
+++ Git & Java/Homework/src/Pattern_Matcher.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Pattern_Matcher {
@@ -11,12 +8,17 @@ public class Pattern_Matcher {
         String s = input[0];
         String p = input[1];
 
-        if (p.charAt(0) == '*') {
+        if (p.matches("^[*]*$")) {
+            System.out.println(true);
+            return;
+        }
+
+        if (p.charAt(0) == '*' && p.substring(1).length() > 0) {
             p = p.substring(1);
         }
 
-        if (p.charAt(p.length() - 1) == '*') {
-            p = p.substring(0, p.length() - 2);
+        if (p.charAt(p.length() - 1) == '*' && (p.substring(0, p.length() - 1).length() > 0)) {
+            p = p.substring(0, p.length() - 1);
         }
 
         System.out.println(findMatch(s, p, 0, 0));
